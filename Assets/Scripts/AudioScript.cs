f3925e7a763a43dd410024a1e9e07fdce4810593
Boxbard <54591758+Boxbard@users.noreply.github.com>
2019-10-06 17:59:00 -0400

    Tutorial 1.5, animation and sound
    Tutorial for making a 2D character walk or run, as well as adding sounds.

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            musicSource.loop = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            musicSource.loop = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+
+    // The code above this comment creates an Animator variable called "anim" so that we can do things with it later;
+    //the code below it in the Start function sets the value of our anim variable using our Animator component in Unity
+
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+       
+    // The code below here sets the Integer value of our anim variable based on whether we have the W key
+    //pressed down or not. If it is pressed down, the State integer gets set to 1; when it is released,
+    //we set it back to 0.  We do something similar for the running animation with the Left Shift key.
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+    }
+}
